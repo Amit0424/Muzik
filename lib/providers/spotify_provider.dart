82f00c213d66915d2aab7d3a3fdbbcee37005f36
Amit <amitjat2406@gmail.com>
2024-08-21 21:54:16 +0530

    API: search API implement

--- lib/providers/spotify_provider.dart
+++ lib/providers/spotify_provider.dart
@@ -6,18 +6,25 @@ import 'package:spotify_sdk/spotify_sdk.dart';
 
 class SpotifyProvider with ChangeNotifier {
   String artistId = '2w9zwq3AktTeYYMuhMjju8';
+  String searchType =
+      'multi'; // albums, artists, episodes, genres, playlists, podcasts, tracks and users
+  int searchOffset = 0;
+  int searchLimit = 10;
+  int searchNumberOfTopResults = 5;
+  String searchQuery = '';
+  final _headers = {
+    'x-rapidapi-key': '55e14a6570msh883814e43f8fa44p1c6f5fjsncf24047ddd09',
+    'x-rapidapi-host': 'spotify23.p.rapidapi.com',
+    'X-RapidAPI-Mock-Response': '200'
+  };
 
   getArtist() async {
+    if (artistId.isEmpty) return;
     try {
       final uri =
           Uri.parse('https://spotify23.p.rapidapi.com/artists/?ids=$artistId');
-      final headers = {
-        'x-rapidapi-key': '55e14a6570msh883814e43f8fa44p1c6f5fjsncf24047ddd09',
-        'x-rapidapi-host': 'spotify23.p.rapidapi.com',
-        'X-RapidAPI-Mock-Response': '200'
-      };
 
-      final response = await http.get(uri, headers: headers);
+      final response = await http.get(uri, headers: _headers);
 
       log(response.body);
     } catch (e) {
@@ -25,9 +32,22 @@ class SpotifyProvider with ChangeNotifier {
     }
   }
 
-  playSpotify() async {
-    log('Playing Spotify');
-    await SpotifySdk.play(spotifyUri: 'spotify:track:7EFfeGN5U0zm9C6KpOeNBH');
+  search() async {
+    if (searchQuery.isEmpty || searchType.isEmpty) return;
+    try {
+      final uri = Uri.parse(
+          'https://spotify23.p.rapidapi.com/search/?q=$searchQuery&type=$searchType&offset=$searchOffset&limit=$searchLimit&numberOfTopResults=$searchNumberOfTopResults');
+
+      final response = await http.get(uri, headers: _headers);
+
+      log(response.body);
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  playTrack(String trackId) async {
+    await SpotifySdk.play(spotifyUri: trackId);
   }
 
   void pauseTrack() async {
